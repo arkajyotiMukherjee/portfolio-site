4cb17a4de93958f12b5288250fb610a5f0ba4d6a
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2021-04-05 00:01:17 +0530

    added jest-style-components library to improve test snapshots
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/components/button/__tests__/button.test.tsx
+++ src/components/button/__tests__/button.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import "jest-styled-components";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "../../../styles/theme";
--- src/components/link/__tests__/link.test.tsx
+++ src/components/link/__tests__/link.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import "jest-styled-components";
 import React from "react";
 import { Link } from "../Link";
 
